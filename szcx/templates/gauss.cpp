cf98a3bff4d8f8daa248e8710c9df1bdc45ad8ce
Wei <2o18o424@gmail.com>
2019-03-16 09:44:59 +0800

    Update templates again

--- szcx/templates/gauss.cpp
+++ szcx/templates/gauss.cpp
@@ -1,19 +1,21 @@
-	for(i=0;i<n;i++){
-        int mk=-1;double m=-1;
-        for(j=0;j<n;j++)if(!v[j]&&fabs(ma[j][i])>m)m=fabs(ma[j][i]),mk=j;
-        f[i]=mk;v[mk]=1;
-        for(j=0;j<n;j++)if(fabs(ma[j][i])>1e-10&&!v[j])
-            for(k=n;k>=i;k--)
-                ma[j][k]=ma[mk][k]-ma[mk][i]/ma[j][i]*ma[j][k];
-    }
-    for(int ni=n-1;ni>=0;ni--){
-        double &k=x[ni];
-        int i=f[ni];
-        k=ma[i][n];
-        for(j=ni+1;j<n;j++)k-=ma[i][j]*x[j];
-        k/=ma[i][ni];
-        if(k!=k||k>1e10||k<-1e10){
-            puts("No Solution");
-            return 0;
-        }
-    } 
+void gauss(){
+	int f[1010]={},v[1010]={};
+	for(int i=1;i<=n;i++){
+		int mk=-1;
+		for(int j=1;j<=n;j++)if(!v[j]&&ma[j][i])mk=j;
+		f[i]=mk;v[mk]=1;
+		for(int j=1;j<=n;j++)if(ma[j][i]&&!v[j]){
+			ll p=ma[mk][i]*po(ma[j][i],mod-2)%mod;
+			for(int k=n+1;k>=i;k--)
+				ma[j][k]=(ma[mk][k]+mod-p*ma[j][k]%mod)%mod;
+		}
+	}
+/*	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n+1;j++)
+			printf("%lld%c",ma[i][j]," \n"[j>n]);*/
+	for(int ni=n;ni;ni--){
+		int i=f[ni];x[ni]=ma[i][n+1];
+		for(int j=ni+1;j<=n;j++)x[ni]=(x[ni]+mod-ma[i][j]*x[j]%mod)%mod;
+		(x[ni]*=po(ma[i][ni],mod-2))%=mod;
+	}
+}
